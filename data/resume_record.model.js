2f68ccb8d50e0bf388c163009dee930e1ae89505
JoshuaDESKTOP <jc.grant22@gmail.com>
2020-05-07 18:12:45 -0700

    Got server working

---
+++ data/resume_record.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let ResumeRecord = new Schema({
+    record_type: {
+        type: String
+    },
+    record_title: {
+        type: String
+    },
+    record_date: {
+        type: String
+    },
+    record_text: {
+        type: String
+    },
+    record_priority: {
+        type: Number
+    }
+})
+
+module.exports = mongoose.model("resume_records", ResumeRecord);
---
+++ server.js
@@ -0,0 +1,35 @@
+const ResumeRecord = require('./data/resume_record.model.js');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/jdw', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.listen(PORT, function() {
+    console.log("Server running on port " + PORT);
+});
+
+const routes = express.Router();
+app.use("/api", routes);
+
+routes.route('/').get((req, res) => {
+    ResumeRecord.find((err, records) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(records);
+        }
+    });
+});
