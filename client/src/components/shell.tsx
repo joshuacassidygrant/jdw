d9ff9df6dbf941c5c4ed553888e8dfe4f3213686
JoshuaDESKTOP <jc.grant22@gmail.com>
2020-05-29 16:30:24 -0700

    logo fix

--- client/src/components/shell.tsx
+++ client/src/components/shell.tsx
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 export default class Shell extends Component {
 
     render() {
-        return(
+        return (
             <div className="shell">
                 {this.props.children}
             </div>
