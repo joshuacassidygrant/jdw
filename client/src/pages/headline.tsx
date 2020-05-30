facb595305f740a0068f948b9232967f37b1c5fd
JoshuaDESKTOP <jc.grant22@gmail.com>
2020-05-30 12:55:47 -0700

    test

--- client/src/pages/headline.tsx
+++ client/src/pages/headline.tsx
@@ -7,13 +7,17 @@ export default class Headline extends Component {
     render () {
         return (
             <div className="headline">
-                <LineDrawer width = {1040} height={256} frequency={50}/>
                 <h1>
                     <strong>Joshua Grant</strong> is a <strong>developer</strong>, <strong>writer</strong> and <strong>designer </strong>looking for work in <strong>Toronto</strong> or <strong> online</strong>.
                 </h1>
-                Check out my <Link to="/resume">resume</Link> or <Link to="/contact">contact me</Link>  here.
             </div>
         )
     }
+
+    //                  <LineDrawer width = {1040} height={256} frequency={50}/>
+
+
+    //                 Check out my <Link to="/resume">resume</Link> or <Link to="/contact">contact me</Link>  here.
+
 
 }
