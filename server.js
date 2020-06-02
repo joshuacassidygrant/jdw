79457ab2c5eab7b4fa9912af7bf55e7f3c3dbd0b
JoshuaDESKTOP <jc.grant22@gmail.com>
2020-06-02 16:57:57 -0700

    maybe fixed routing

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ api.route('/send').post((req, res) => {
   })
 
   app.get('/*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'client/index.html'));
+    res.sendFile(path.resolve('./client/index.html'));
   }); 
   
 
